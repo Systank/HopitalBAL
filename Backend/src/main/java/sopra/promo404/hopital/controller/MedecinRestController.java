79fe44207708f9fd99ac679c72cf35070674bc50
AlixJo <alix-jourdan@hotmail.fr>
2018-09-11 15:39:34 +0200

    ControllerRest de medecin et specialite

---
+++ Backend/src/main/java/sopra/promo404/hopital/controller/MedecinRestController.java
@@ -0,0 +1,74 @@
+package sopra.promo404.hopital.controller;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import sopra.promo404.formation.model.Views;
+import sopra.promo404.hopital.model.Medecin;
+import sopra.promo404.hopital.repository.IRepoMedecin;
+
+
+
+@RestController
+@RequestMapping("/medecin")
+public class MedecinRestController {
+
+	@Autowired
+	private IRepoMedecin medecinRepo;
+
+	@GetMapping("")
+	@ResponseBody
+	@JsonView(Views.ViewMedecin.class)
+	public List<Medecin> list() {
+		return medecinRepo.findAllMedecin();
+	}
+
+	@GetMapping("/{id}")
+	@ResponseBody
+	@JsonView(Views.ViewMedecinWithSpecialite.class)
+	public Medecin detail(@PathVariable Long id) {
+		return medecinRepo.findMedecinByIdWithSpecialite(id);
+	}
+
+	@PostMapping("")
+	@ResponseBody
+	@JsonView(Views.ViewMedecin.class)
+	public Medecin add(@RequestBody Medecin medecin) {
+		medecinRepo.save(medecin);
+
+		return medecin;
+	}
+
+	@PutMapping("/{id}")
+	@ResponseBody
+	@JsonView(Views.ViewMedecin.class)
+	public Medecin edit(@RequestBody Medecin medecin, @PathVariable Long id) {
+		medecinRepo.save(medecin);
+
+		return (Medecin) medecinRepo.findById(id).get();
+	}
+
+
+	@DeleteMapping("/{id}")
+	@JsonView(Views.ViewMedecin.class)
+	public void delete(@PathVariable Long id) {
+		medecinRepo.deleteById(id);
+	}
+
+}
---
+++ Backend/src/main/java/sopra/promo404/hopital/controller/SpecialiteRestController.java
@@ -0,0 +1,65 @@
+package sopra.promo404.hopital.controller;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import sopra.promo404.hopital.model.Specialite;
+import sopra.promo404.hopital.repository.IRepoSpecialite;
+
+
+@RestController
+@RequestMapping("/specialite")
+public class SpecialiteRestController {
+	@Autowired
+	private IRepoSpecialite specialiteRepo;
+
+	@GetMapping("")
+	@ResponseBody
+	@JsonView(Views.ViewSpecialite.class)
+	public List<Specialite> list() {
+		return specialiteRepo.findAllSpecialite();
+	}
+
+
+	@PostMapping("")
+	@ResponseBody
+	@JsonView(Views.ViewSpecialite.class)
+	public Specialite add(@RequestBody Specialite specialite) {
+		specialiteRepo.save(specialite);
+
+		return specialite;
+	}
+
+	@PutMapping("/{id}")
+	@ResponseBody
+	@JsonView(Views.ViewSpecialite.class)
+	public Specialite edit(@RequestBody Specialite specialite, @PathVariable Long id) {
+		specialiteRepo.save(specialite);
+
+		return (Specialite) specialiteRepo.findById(id).get();
+	}
+
+
+	@DeleteMapping("/{id}")
+	@JsonView(Views.ViewSpecialite.class)
+	public void delete(@PathVariable Long id) {
+		specialiteRepo.deleteById(id);
+	}
+
+}
